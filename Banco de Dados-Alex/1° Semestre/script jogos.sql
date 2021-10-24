170132cdc4b05e0306789bddf06fe1b9a28cbf2a
ViniciusFonseca-18 <vinicius.barbosa@bandtec.com.br>
2021-10-24 15:03:43 -0300

    atualização

---
+++ Banco de Dados-Alex/1° Semestre/script jogos.sql
@@ -0,0 +1,13 @@
+-- escolher um jogo
+-- criar minimo 3 no maximo 5 tabelas 
+-- tabelas serão relacionadas
+-- será contabilizado como tabelas associativas
+-- é obrigatório ter uma associativas 
+-- maior relacionamento nas tabelas 
+-- escrever as regras do jogos
+
+create database clash royale;
+
+use clash royale;
+
+create table clash of clans
